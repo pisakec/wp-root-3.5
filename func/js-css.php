7a8a79b678f5351df83d5b5fa70c14d419679b07
pisakec <pisakec@yahoo.com>
2019-07-31 10:56:40 +0700

    Update js-css.php

--- func/js-css.php
+++ func/js-css.php
@@ -77,20 +77,7 @@ function theme_styles() {
 //	}
 }
 add_action( 'wp_print_styles', 'theme_styles',99 );
-/* ==========================================================================
-//rel=’preload’ Attribute
-========================================================================== */
-function add_rel_preload($html, $handle, $href, $media) {
-    
-    if (is_admin())
-        return $html;
 
-     $html = <<<EOT
-<link rel='preload' as='style' onload="this.onload=null;this.rel='stylesheet'" id='$handle' href='$href' type='text/css' media='all' />
-EOT;
-    return $html;
-}
-add_filter( 'style_loader_tag', 'add_rel_preload', 10, 4 );
 /* ==========================================================================
 // remove wp version param from any enqueued scripts
 ========================================================================== */
