aca89a3ab6b66387b2c3e06a382913cc37c39324
pisakec <pisakec@yahoo.com>
2020-02-24 11:45:20 +0700

    Add Automatically link featured post thumbnails to their original posts

--- func/display-featured-img-admin.php
+++ func/display-featured-img-admin.php
@@ -35,4 +35,14 @@ function my_custom_fonts() {
     } 
   </style>';
 }
+
+/* ==========================================================================
+// Automatically link featured post thumbnails to their original posts
+========================================================================== */
+
+function wcs_auto_link_post_thumbnails( $html, $post_id, $post_image_id ) {
+$html = '<a href="' . get_permalink( $post_id ) . '" title="' . esc_attr( get_the_title( $post_id ) ) . '">' . $html . '</a>';
+return $html;
+}
+add_filter( 'post_thumbnail_html', 'wcs_auto_link_post_thumbnails', 10, 3 );
 ?>
