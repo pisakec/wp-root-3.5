cbed82a65b5c977a33e87877063f20efd164cc2b
pisakec <pisakec@yahoo.com>
2026-01-09 10:20:35 +0700

    blah, blah, blah
    Updated category.php to enhance layout and content display.

--- category.php
+++ category.php
@@ -5,11 +5,12 @@ get_header();
 
 <main id="category" id="post-<?php the_ID(); ?>">
 	<section class="content ">
-		<div class="wrapper row-wrap">
-			<div class="has-sidebar row-wrap">
-				<h1 class="text-align-left full-width"><?php $cat = get_the_category(); echo $cat[0]->cat_name; ?></h1>
+		<div class="wrapper row-wrap ">
+			
+				<h1 class="text-align-left full-width"><?php single_cat_title(); ?></h1>
+				
 				<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-				<article <?php post_class(); ?>>
+				<article <?php post_class('column-3'); ?>>
 					<div class="featured-image">
 						<a href="<?php the_permalink() ?>">
 							<?php $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );?>
@@ -17,16 +18,16 @@ get_header();
 						</a>
 					</div>
 					<div class="entry">
-						<h2 class="header-m header-500"><a href="<?php the_permalink()?>" class=""><?php the_title(); ?></a> </h2>
+						<h2 class="header-5 header-700 full-width text-align-left"><a href="<?php the_permalink()?>" class=""><?php the_title(); ?></a> </h2>
 						<p>
-							<?php echo wp_trim_words( get_the_content(), 40, '...' ); ?> </p>
+							<?php echo wp_trim_words( get_the_content(), 20, '...' ); ?> </p>
+						<a href="<?php the_permalink() ?>" rel="bookmark" class="read-more">Read More</a>
 					</div>
-					<a href="<?php the_permalink() ?>" rel="bookmark" class="read-more">Read More</a>
+					
 				</article>
 				<?php endwhile; endif; ?>
 				<?php //wp_pagenavi(); ?>
-			</div>
-			<?php get_sidebar(); ?>
+		
 		</div>
 	</section>
 </main>
